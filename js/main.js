ce17257f270e9311f7dc87dbf4de79c0f45173e4
thisNiharika <missrathoratwork@gmail.com>
2022-07-02 19:04:47 +0530

    image change

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 for (i=0; i<4;i++ )
 {
     var img =document.createElement("img");
-    img.src = "images/image"+(i+1)+".jpg"; 
+    img.src = "Images/Image"+(i+1)+".jpg"; 
 img.width=300;
 img.height=300;
 var src = document.getElementById("x"+i);
